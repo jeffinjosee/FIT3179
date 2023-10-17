5e66c0164bee6846dc98ea1b798b8d2c752f35ea
jeffinjosee <142390185+jeffinjosee@users.noreply.github.com>
2023-10-17 07:02:05 +0530

    Add files via upload

---
+++ top10_2021.js
@@ -0,0 +1,42 @@
+{
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "title": {"text": "Top 10 Countries with Highest Life Expectancy in 2021"},
+    "width": 400,
+    "height": 400,
+    "description": "Life Expectancy in 2021",
+    "data": {
+      "url": "https://raw.githubusercontent.com/jeffinjosee/FIT3179/main/LifeExpectancy2021.csv",
+      "format": {
+        "type": "csv"
+      }
+    },
+    "mark": {
+      "type": "bar",
+      "color": "#31a354"
+    },
+    "encoding": {
+      "x": {
+        "field": "Country",
+        "type": "nominal",
+        "title": "Top 10 Countries"
+      },
+      "y": {
+        "field": "Life Expectancy at Birth (2021)",
+        "type": "quantitative",
+        "scale": {"domain": [80, 86], "clamp": true}
+      },
+      "tooltip": [
+        {"field": "Country", "title": "Country"},
+        {"field": "Life Expectancy at Birth (2021)", "title": "Life Expectancy (2021)"}
+      ]
+    },
+    "transform": [
+      {
+        "window": [{"op": "rank", "as": "rank"}],
+        "sort": [{"field": "Life Expectancy at Birth (2021)", "order": "descending"}]
+      },
+      {
+        "filter": {"field": "rank", "lte": 10}
+      }
+    ]
+  }
